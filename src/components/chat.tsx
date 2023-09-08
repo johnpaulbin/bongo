7010e8aa0a36fa3599939d5a66ca6bf637f676ed
johnpaulbin <johnpaulbin@gmail.com>
2023-09-07 18:01:01 -0700

    Update chat.tsx

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -80,7 +80,7 @@ export default function Chat({ className }: ChatProps) {
                       className="typing-control-item stop"
                     >
                       <SVG alt="stop" src={StopIcon} width={24} className="mr-1" />
-                      <span>停止响应</span>
+                      <span>stopped responding</span>
                     </button>
                   </div>
                 ) : null}
